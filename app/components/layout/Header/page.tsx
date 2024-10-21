45dd66db1df76e65cef5df13986d6af36f75f9d4
hery.heryanto21 <hery.heryanto21@gmail.com>
2024-10-21 01:50:21 +0000

    delete navlink

--- app/components/layout/Header/page.tsx
+++ app/components/layout/Header/page.tsx
@@ -1,11 +1,10 @@
 
 'use client'
-
+import Link from 'next/link'
 import { useRouter, usePathname } from 'next/navigation'
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import CompanyProfile from '../../elements/Navbar/CompanyProfile/page'
-import Navlink from '../../elements/Navbar/Navlink/page'
 import UserProfil from '../../elements/Navbar/UserProfil/page'
 
 function classNames(...classes: (string | undefined | null | false)[]): string {
@@ -38,7 +37,24 @@ export default function Header() {
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <CompanyProfile />
-              <Navlink navigation={navigation} />
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      aria-current={item.current ? 'page' : undefined}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-sm font-medium',
+                      )}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <button
