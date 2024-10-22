1fa8239670383f0ff9f7007bb3e9ba19e26e24e4
hery.heryanto21 <hery.heryanto21@gmail.com>
2024-10-22 09:02:44 +0000

    Use 'const' instead

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,13 @@
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react"
+
 
 export default async function About() {
 
-  let data = await fetch('https://api.vercel.app/blog')
-  let posts = await data.json()
+const data = await fetch('https://api.vercel.app/blog')
+const posts = await data.json()
 
   return (
     <ul>
-      {posts.map((post: { id: Key | null | undefined; title: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined }) => (
+      {posts.map((post:any) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ul>
