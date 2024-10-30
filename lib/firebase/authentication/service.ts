6bc91124d5a34dd473423622b5f5c948d1feb89b
Hery Heryanto <77720886+h3ryanto@users.noreply.github.com>
2024-10-30 21:43:59 +0700

    Update service.ts
    Update redirect logout

--- lib/firebase/authentication/service.ts
+++ lib/firebase/authentication/service.ts
@@ -1,6 +1,6 @@
 
 import app from '../init'
-import { Redirect } from 'next/navigation'
+import { redirect } from 'next/navigation'
 import { createSession,deleteSession} from '@/lib/auth/session'
 import {
     getAuth,
@@ -36,7 +36,7 @@ export const Authentication = () => {
   export const SignOut = async () => {
     await signOut(FirebaseAuth)
     deleteSession()
-    Redirect('/login') 
+    redirect('/login') 
   }
   
   export const GetSignInErrorMessage = (code:any) => {
