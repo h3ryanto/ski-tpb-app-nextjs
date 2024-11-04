fd3699485abef1ad1397abb16ff32f75aaebfe5e
h3ryanto <hery.heryanto21@gmail.com>
2024-11-04 17:00:49 +0700

    pagination

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,56 @@
-import type { Metadata } from 'next'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client"
+import React, { useState, useEffect } from 'react';
+import { getNextsPaginatedData, getFirstsPaginatedData, retriveData } from '@/lib/firebase/firestore/service';
 
-export const metadata: Metadata = {
-  title: 'Dashboard',
-}
+export default function PaginationComponent() {
+  const [data, setData] = useState([]);
+  const [lastVisible, setLastVisible] = useState(null);
+  const [firstVisible, setFirstVisible] = useState(null);
 
-export default async function Dashboard() {
+  const load = async () => {
+    const data: any = await retriveData('header');
+    const newLastVisible = data.newLastVisible;
+    setData(data.data);
+    setLastVisible(newLastVisible);
+  }
+  const loadNext = async () => {
+    const data: any = await getNextsPaginatedData(lastVisible);
+    const newLastVisible = data.newLastVisible;
+    const newFirstVisible = data.newFirstVisible;
+    setData(data.data);
+    setLastVisible(newLastVisible);
+    setFirstVisible(newFirstVisible);
+    console.log(data.newFirstVisible);
+  };
 
-  const data = await fetch('https://api.vercel.app/blog')
-  const posts = await data.json()
+  const loadPrev = async () => {
+    console.log(firstVisible);
+    const data: any = await getFirstsPaginatedData(firstVisible);
+    const newFirstVisible = data.newfirstVisible;
+    const newLastVisible = data.newLastVisible;
+    setData(data.data);
+    setFirstVisible(newFirstVisible);
+    setLastVisible(newLastVisible);
+
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
 
   return (
-    <ul>
-      {posts.map((post: { id: string; title: string }) => (
-        <li key={post.id}>{post.title}</li>
+    // <div>test</div>
+    <div className="max-w-sm">
+      {data.map((item: any) => (
+        <div key={item.id}>{item.nomor_aju}</div>
       ))}
-    </ul>
-  )
+      <div className="relative flex h-16 items-center justify-between max-w-sm">
+        <button onClick={loadPrev}>Prev</button>
+        <button onClick={loadNext}>Next</button>
+      </div>
+
+    </div>
+  );
 }
--- lib/firebase/firestore/service.ts
+++ lib/firebase/firestore/service.ts
@@ -1,44 +1,66 @@
-import { collection, getDocs, getFirestore, setDoc, doc, deleteDoc, query, orderBy, limit, startAfter } from "firebase/firestore";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { collection, getDocs, getFirestore, setDoc, doc, deleteDoc, query, orderBy, limit, startAfter, endBefore } from "firebase/firestore";
 import app from "../init";
 
 const firestore = getFirestore(app);
 
 export async function retriveData(collectionName: string,) {
-    const first = query(collection(firestore, collectionName), orderBy("nomor_aju"), limit(25));
+    const first = query(collection(firestore, collectionName), orderBy("nomor_aju"), limit(10));
 
-    // const snapshot = await getDocs(collection(firestore, collectionName));
-    const documentSnapshots = await getDocs(first);
-
-    const lastVisible = documentSnapshots.docs[documentSnapshots.docs.length-1];
-    console.log("last", lastVisible);
-
-    const next= query(collection(firestore, collectionName),    
-    orderBy("nomor_aju"),
-    startAfter(lastVisible),
-    limit(25));
-    
-    const snapshot = await getDocs(next);
+    const snapshot = await getDocs(first);
+    const newLastVisible = snapshot.docs[snapshot.docs.length-1];
     const data = snapshot.docs.map((doc:any) => ({
         id: doc.id,
         ...doc.data(),
     }));
 
-    return data;
+    return {data,newLastVisible};
 }
 
-export async function addData() {
-    const data = {
-        user: "data baru ni booss",
-        user_name: "CA",
-        is_admin: "USA",
-        filed_baru: "coba"
-    }
-    const newUser = doc(collection(firestore, "user"));
-    // Add a new document in collection "cities"
-    await setDoc(newUser, data)
-}
 
-export async function Del() {
-    await deleteDoc(doc(firestore, "user", "eLo28n2rSBhzdvAzkGmq"));
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function getNextsPaginatedData(lastVisible:any) {
+
+      const query1:any = query(collection(firestore, 'header'), orderBy("created_at", "desc"), startAfter(lastVisible), limit(10));
+      const snapshot = await getDocs(query1);
+      const newLastVisible = snapshot.docs[snapshot.docs.length-1];
+      const newFirstVisible = snapshot.docs[0];
+    const data = snapshot.docs.map((doc:any) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    return { data, newLastVisible,newFirstVisible };
+    
+  };
+
+
+  export async function getFirstsPaginatedData(firstVisible:any) {
+
+    const query1:any = query(collection(firestore, 'header'), orderBy("created_at", "desc"), endBefore(firstVisible), limit(10));
+    const snapshot = await getDocs(query1);
+    const newLastVisible = snapshot.docs[snapshot.docs.length-1];
+    const newFirstVisible = snapshot.docs[0];
+  const data = snapshot.docs.map((doc:any) => ({
+      id: doc.id,
+      ...doc.data(),
+  }));
+  return { data, newLastVisible,newFirstVisible };
+  
+};
+
+// export async function addData() {
+//     const data = {
+//         user: "data baru ni booss",
+//         user_name: "CA",
+//         is_admin: "USA",
+//         filed_baru: "coba"
+//     }
+//     const newUser = doc(collection(firestore, "user"));
+//     // Add a new document in collection "cities"
+//     await setDoc(newUser, data)
+// }
+
+// export async function Del() {
+//     await deleteDoc(doc(firestore, "user", "eLo28n2rSBhzdvAzkGmq"));
+// }
 
