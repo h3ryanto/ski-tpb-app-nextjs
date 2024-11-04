b147aa6f378371966de2941451047d3c2a042cca
hery.heryanto21 <hery.heryanto21@gmail.com>
2024-11-04 16:17:38 +0000

    dashboar pagination

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
+import style from './styles.module.css'
 import React, { useState, useEffect } from 'react';
 import { getNextsPaginatedData, getFirstsPaginatedData, retriveData } from '@/lib/firebase/firestore/service';
 
@@ -19,29 +18,27 @@ export default function PaginationComponent() {
   }
 
   const loadNext = async () => {
-    const data: any = await getNextsPaginatedData(lastVisible,page);
+    const data: any = await getNextsPaginatedData(lastVisible);
     const newLastVisible = data.newLastVisible;
     const newFirstVisible = data.newFirstVisible;
-    const pageNow = data.pageNow;
     setData(data.data);
     setLastVisible(newLastVisible);
     setFirstVisible(newFirstVisible);
     // console.log(data.newFirstVisible);
-    setPage(pageNow);
+    setPage(prev => prev + 1);
     console.log(newFirstVisible._document.data.value.mapValue.fields.nomor_aju)
   };
 
   const loadPrev = async () => {
     // console.log(firstVisible._document.data.value.mapValue.fields.nomor_aju)
-    const data: any = await getFirstsPaginatedData(firstVisible,page);
+    const data: any = await getFirstsPaginatedData(firstVisible);
     const newFirstVisible = data.newfirstVisible;
     const newLastVisible = data.newLastVisible;
-    const pageNow = data.pageNow;
     setData(data.data);
     setFirstVisible(newFirstVisible);
     setLastVisible(newLastVisible);
-    setPage(pageNow);
-    console.log(newLastVisible._document.data.value.mapValue.fields.nomor_aju)
+    setPage(prev => prev - 1);
+    // console.log(newFirstVisible)
   };
 
   useEffect(() => {
@@ -49,17 +46,33 @@ export default function PaginationComponent() {
   }, []);
 
   return (
-    // <div>test</div>
-    <div className="max-w-sm">
-      {data.map((item: any) => (
-        <div key={item.id}>{item.nomor_aju}</div>
-      ))}
-      <div className="relative flex h-16 items-center justify-between max-w-sm">
+    
+    <div className="container mx-auto px-4">
+    <table id={style.table} className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Dok</th>
+          <th scope="col">Nomor Aju</th>
+          <th scope="col">Nomor Daftar</th>
+          <th scope="col">Tanggal Daftar</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data && data.map((post: any) => (
+          <tr key={post.id}>
+            <td>{post.kode_dokumen}</td>
+            <td>{post.nomor_aju}</td>
+            <td>{post.nomor_daftar}</td>
+            <td>{post.tanggal_daftar}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <div className="relative flex h-16 items-center justify-between max-w-sm">
         <button onClick={loadPrev} disabled={page===1?true:false}>Prev</button>
-        <div>{page}</div>
+        <div>Page : {page}</div>
         <button onClick={loadNext}>Next</button>
       </div>
-
-    </div>
+  </div>
   );
 }
--- lib/firebase/firestore/service.ts
+++ lib/firebase/firestore/service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { collection, getDocs, getFirestore, setDoc, doc, deleteDoc, query, orderBy, limit, startAfter, endBefore, endAt } from "firebase/firestore";
+import { collection, getDocs, getFirestore, query, orderBy, limit, startAfter, endBefore, limitToLast} from "firebase/firestore";
 import app from "../init";
 
 const firestore = getFirestore(app);
@@ -18,8 +18,7 @@ export async function retriveData(collectionName: string,) {
 }
 
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function getNextsPaginatedData(lastVisible:any,page:number) {
+export async function getNextsPaginatedData(lastVisible:any) {
 
       const query1:any = query(collection(firestore, 'header'), orderBy("created_at", "desc"), startAfter(lastVisible), limit(10));
       const snapshot = await getDocs(query1);
@@ -31,15 +30,14 @@ export async function getNextsPaginatedData(lastVisible:any,page:number) {
 
     const newLastVisible = snapshot.docs[snapshot.docs.length-1];
     const newFirstVisible = snapshot.docs[snapshot.docs.length-10];
-    const pageNow=page+1;
-    return { data, newLastVisible,newFirstVisible,pageNow };
+    return { data, newLastVisible,newFirstVisible};
     
   };
 
 
-export async function getFirstsPaginatedData(firstVisible:any,page:number) {
+export async function getFirstsPaginatedData(firstVisible:any) {
 
-    const retrevePrev:any = query(collection(firestore, 'header'), orderBy("created_at", "desc"), endBefore(firstVisible), limit(10));
+    const retrevePrev:any = query(collection(firestore, 'header'), orderBy("created_at", "desc"), endBefore(firstVisible), limitToLast(10));
     const snapshotPrev = await getDocs(retrevePrev);    
     const data = snapshotPrev.docs.map((doc:any) => ({
       id: doc.id,
@@ -47,10 +45,9 @@ export async function getFirstsPaginatedData(firstVisible:any,page:number) {
     }));
 
     const newLastVisible = snapshotPrev.docs[snapshotPrev.docs.length-1];
-    const newFirstVisible = snapshotPrev.docs[snapshotPrev.docs.length-10];
-    const pageNow=page-1;
+    const newFirstVisible = snapshotPrev.docs[0];
   
-    return { data, newLastVisible,newFirstVisible,pageNow };
+    return { data, newLastVisible,newFirstVisible };
   
   };
 
