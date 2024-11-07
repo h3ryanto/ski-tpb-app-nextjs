d8995abe000c5aaf210ac5a5044a0b6c7402d53d
h3ryanto <hery.heryanto21@gmail.com>
2024-11-07 17:04:37 +0700

    next auth login

--- components/elements/Navbar/UserProfil/page.tsx
+++ components/elements/Navbar/UserProfil/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import Image from 'next/image'
 import { SignOut } from '@/lib/firebase/authentication/service'
