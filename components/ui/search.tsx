3e3940210989ae2097d5d0c6d183fd24e5654311
h3ryanto <hery.heryanto21@gmail.com>
2024-12-15 20:30:45 +0700

    add search

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,8 +1,14 @@
 'use client'
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { ReactNode } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search() {
+export default function Search({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const childern = children;
     const searchParams = useSearchParams();
     const pathName = usePathname();
     const { replace } = useRouter();
@@ -17,21 +23,24 @@ export default function Search() {
         replace(`${pathName}?${params.toString()}`)
     }, 300);
     return (
-        <div className=' bg-slate-700 px-2 py-2 md:bg-inherit border-b-2 border-slate-400'>
-            <label className="flex flex-row-reverse w-full ">
-                <input
-                    id="search"
-                    type="text"
-                    name="search"
-                    placeholder="Search.."
-                    autoComplete="current-search"
-                    onChange={(e) => onSearch(e.target.value)}
-                    defaultValue={searchParams.get("query")?.toString()}
-                    className="w-auto rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300
+        <>
+            <div className=' bg-slate-700 px-2 py-2 md:bg-inherit border-b-2 border-slate-400'>
+                <label className="flex flex-row-reverse w-full ">
+                    <input
+                        id="search"
+                        type="text"
+                        name="search"
+                        placeholder="Search.."
+                        autoComplete="current-search"
+                        onChange={(e) => onSearch(e.target.value)}
+                        defaultValue={searchParams.get("query")?.toString()}
+                        className="w-auto rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300
                 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-300
                 text-sm px-2 focus:w-full"
-                />
-            </label>
-        </div>
+                    />
+                </label>
+            </div>
+            {childern}
+        </>
     )
 }
