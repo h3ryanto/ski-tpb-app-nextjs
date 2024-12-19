caf63c7d0bdb16a4c8d5b79d16b7f021d8bfc418
Hery Heryanto <77720886+h3ryanto@users.noreply.github.com>
2024-12-19 23:37:24 +0700

    Update search.tsx

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -34,7 +34,7 @@ export default function Search({ children }: Props) {
                         placeholder="Search.."
                         autoComplete="current-search"
                         onChange={(e) => onSearch(e.target.value)}
-                        defaultValue={searchParams.get('query')?.toString()}
+                        defaultValue={params && searchParams.get('query')?.toString()}
                         className="w-auto rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300
                 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-300
                 text-sm px-2 focus:w-full"
