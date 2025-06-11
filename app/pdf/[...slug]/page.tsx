cbb0519b7611612a1e6e61338a2a80a9cde37c6e
h3ryanto <hery.heryanto21@gmail.com>
2025-06-11 07:34:11 +0700

    split url pdf cloudinary

--- app/pdf/[...slug]/page.tsx
+++ app/pdf/[...slug]/page.tsx
@@ -3,24 +3,24 @@ export default async function PdfView({
   params,
 }: {
   params: Promise<{ slug: string }>
-}){
-        const { slug } = await params
-        const data = await fetch(`${process.env.URL}/api/getPdf?q=${slug[2]}&path=Documens/${slug[0]}/${slug[1]}`)
-        const result = await data.json()
+}) {
+  const { slug } = await params
+  const data = await fetch(`${process.env.URL}/api/getPdf?q=${slug[2]}&path=Documens/${slug[0]}/${slug[1]}`)
+  const result = await data.json()
 
-        if ((result.url) && (data.status === 200)) {
-            console.log(result.url)
-      return (
-        <div className="w-screen h-svh mx-auto flex flex-col items-center">
-            <iframe src={result?.url} className='aspect-square w-full h-full'></iframe>
-        </div>
-    )      
-        }else {
-      return (
-        <div className="flex items-center justify-center w-screen h-svh">
-            <div className="font-bold text-xl">Oops! File Not Found.</div>
-        </div>
-      )
-    }
-    
+  if ((result.url) && (data.status === 200)) {
+    const url = `https://${result.url.split('//')[1]}`
+    return (
+      <div className="w-screen h-svh mx-auto flex flex-col items-center">
+        <iframe src={url} className='aspect-square w-full h-full'></iframe>
+      </div>
+    )
+  } else {
+    return (
+      <div className="flex items-center justify-center w-screen h-svh">
+        <div className="font-bold text-xl">Oops! File Not Found.</div>
+      </div>
+    )
+  }
+
 }
