16d1e4b6865ff5344d02716bef310b22a8eacb9b
Hery Heryanto <77720886+h3ryanto@users.noreply.github.com>
2025-09-25 00:12:51 +0000

    bind mount and revise repository

--- app/api/check-pdf/route.ts
+++ app/api/check-pdf/route.ts
@@ -5,23 +5,23 @@ import path from "path";
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const filename = searchParams.get("file");
-  const document = searchParams.get("document");
-  const year = searchParams.get("year");
+  const kode_dokumen = searchParams.get("document");
+  const tahun = searchParams.get("year");
 
   if (!filename) {
     return NextResponse.json({ error: "File parameter is required" }, { status: 400 });
   }
 
-  if (!document) {
+  if (!kode_dokumen) {
     return NextResponse.json({ error: "Document parameter is required" }, { status: 400 });
   }
 
-  if (!year) {
+  if (!tahun) {
     return NextResponse.json({ error: "Year parameter is required" }, { status: 400 });
   }
 
   // path ke folder public
-  const filePath = path.join(process.cwd(), "public", "repository/2025/40", `${filename}.pdf`);
+  const filePath = path.join(process.cwd(), "public", `repository/${tahun}/${kode_dokumen}`, `${filename}.pdf`);
 
   if (fs.existsSync(filePath)) {
     return NextResponse.json({ exists: true });
