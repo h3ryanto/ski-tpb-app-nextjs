905e2b0f19adbab91278f9b610d459dd6874fd5c
Hery Heryanto <77720886+h3ryanto@users.noreply.github.com>
2025-09-25 09:05:15 +0000

    Path name from pdf viwer

--- app/pdf/[...slug]/page.tsx
+++ app/pdf/[...slug]/page.tsx
@@ -1,18 +1,19 @@
 
+import path from "path";
+
 export default async function PdfView({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
-  // const data = await fetch(`${process.env.URL}/api/getPdf?q=${slug[2]}&path=Documens/${slug[0]}/${slug[1]}`)
-  // const result = await data.json()
+          const filePath = path.join(process.cwd(), `repository/${slug[0]}/${slug[1]}`, `${slug[2]}.pdf`);
+          console.log("File Path:", filePath);
 
-  // if ((result.url) && (data.status === 200)) {
-  //   // const url = `https://${result.url.split('//')[1]}`
   return (
     <div className="w-screen h-svh mx-auto flex flex-col items-center">
-      <iframe src={`${process.env.URL}/api/getPdf/${slug[0]}/${slug[1]}/${slug[2]}`} className='aspect-square w-full h-full'></iframe>
+      <iframe src={filePath} className='aspect-square w-full h-full'></iframe>
+      {/* <iframe src={`${process.env.URL}/api/getPdf/${slug[0]}/${slug[1]}/${slug[2]}`} className='aspect-square w-full h-full'></iframe> */}
       {/* <embed
         src={`${process.env.URL}/api/getPdf/${slug[0]}/${slug[1]}/${slug[2]}`}
         type="application/pdf"
--- components/ui/app-pdf-link-icon.tsx.tsx
+++ components/ui/app-pdf-link-icon.tsx.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import { FileTextIcon } from "lucide-react";
-import { decodeFormState } from "next/dist/server/app-render/entry-base";
+import path from "path";
 
 export default function AppPdfLinkIcon({ nomor_daftar, tahun, kode_dokumen }: { nomor_daftar: any, tahun: string, kode_dokumen: any }) {
     const [pdfExists, setPdfExists] = useState<boolean | null>(null);
-
+    const [pdfLinkPath, setPdfLinkPath] = useState<string>("");
     useEffect(() => {
+
         const cek_pdf = async (file_name: string, year: string, kode_dokumen: string) => {
             const result = await fetch(`/api/check-pdf?file=${file_name}&document=${kode_dokumen}&year=${year}`);
             const status = await result.json();
             return status.exists;
         };
-
+        const filePath = path.join(process.cwd(), `repository/${tahun}/${kode_dokumen}`, `${nomor_daftar}.pdf`);
+        setPdfLinkPath(filePath);
         cek_pdf(nomor_daftar, tahun, kode_dokumen).then(setPdfExists);
     }, [nomor_daftar, tahun, kode_dokumen]);
 
@@ -21,7 +23,8 @@ export default function AppPdfLinkIcon({ nomor_daftar, tahun, kode_dokumen }: { 
                 size={16}
                 className="hover:stroke-red-500 cursor-pointer stroke-red-700"
                 onClick={() => {
-                    const pdfUrl = `/repository/${tahun}/${kode_dokumen}/${nomor_daftar}.pdf`;
+                    const pdfUrl = pdfLinkPath;
+                    console.log("Opening PDF:", pdfUrl);
                     window.open(pdfUrl, '_blank');
                 }}
             />
