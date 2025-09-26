4f2f97c7069bce0fd9d14b5145a52caefefe1347
Hery Heryanto <77720886+h3ryanto@users.noreply.github.com>
2025-09-26 07:32:53 +0000

    get repository in laravel tpb-ski-app

--- app/pdf/[...slug]/page.tsx
+++ app/pdf/[...slug]/page.tsx
@@ -1,33 +1,13 @@
 
-import path from "path";
-
 export default async function PdfView({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
-          const filePath = path.join(process.cwd(), `repository/${slug[0]}/${slug[1]}`, `${slug[2]}.pdf`);
-          console.log("File Path:", filePath);
-
   return (
     <div className="w-screen h-svh mx-auto flex flex-col items-center">
-      <iframe src={filePath} className='aspect-square w-full h-full'></iframe>
-      {/* <iframe src={`${process.env.URL}/api/getPdf/${slug[0]}/${slug[1]}/${slug[2]}`} className='aspect-square w-full h-full'></iframe> */}
-      {/* <embed
-        src={`${process.env.URL}/api/getPdf/${slug[0]}/${slug[1]}/${slug[2]}`}
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      /> */}
+      <iframe src={`${process.env.URL}/api/getPdf/${slug[0]}/${slug[1]}/${slug[2]}`} className='aspect-square w-full h-full'></iframe>
     </div>
   )
-  // } else {
-  //   return (
-  //     <div className="flex items-center justify-center w-screen h-svh">
-  //       <div className="font-bold text-xl">Oops! File Not Found.</div>
-  //     </div>
-  //   )
-  // }
-
 }
--- components/ui/app-pdf-link-icon.tsx.tsx
+++ components/ui/app-pdf-link-icon.tsx.tsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react";
-import { FileTextIcon } from "lucide-react";
+import { FileTextIcon, LoaderCircle } from "lucide-react";
 import path from "path";
 
 export default function AppPdfLinkIcon({ nomor_daftar, tahun, kode_dokumen }: { nomor_daftar: any, tahun: string, kode_dokumen: any }) {
     const [pdfExists, setPdfExists] = useState<boolean | null>(null);
+    const [checkLoading, setCheckLoading] = useState<boolean| null>(null);
     const [pdfLinkPath, setPdfLinkPath] = useState<string>("");
     useEffect(() => {
 
         const cek_pdf = async (file_name: string, year: string, kode_dokumen: string) => {
-            const result = await fetch(`/api/check-pdf?file=${file_name}&document=${kode_dokumen}&year=${year}`);
+            setCheckLoading(true);
+            const url = `https://tpb.heryheryanto.my.id/check-pdf/${tahun}/${kode_dokumen}/${nomor_daftar}`;
+            const result = await fetch(url);
             const status = await result.json();
+            console.log("PDF exists:", status);            
+            setCheckLoading(false);
             return status.exists;
         };
-        const filePath = path.join(process.cwd(), `repository/${tahun}/${kode_dokumen}`, `${nomor_daftar}.pdf`);
+        const filePath = path.join(process.cwd(), `pdf/${tahun}/${kode_dokumen}/`, `${nomor_daftar}`);
         setPdfLinkPath(filePath);
         cek_pdf(nomor_daftar, tahun, kode_dokumen).then(setPdfExists);
     }, [nomor_daftar, tahun, kode_dokumen]);
 
     return (
+        checkLoading ? (
+            <LoaderCircle className="animate-spin stroke-red-500 mx-3" size={16} />
+        ) :
         pdfExists ? (
             <FileTextIcon
                 size={16}
