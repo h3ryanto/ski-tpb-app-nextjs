d2d378283247dd13114fbee53f8c1c7b9006e08b
h3ryanto <hery.heryanto21@gmail.com>
2025-10-16 15:00:45 +0700

    deploy

--- app/api/getPdf/[...slug]/route.ts
+++ app/api/getPdf/[...slug]/route.ts
@@ -1,4 +1,3 @@
-// import cloudinary from '@/lib/cloudinary/config';
 import { getToken } from "next-auth/jwt";
 
 export async function GET(
@@ -9,38 +8,21 @@ export async function GET(
     const { slug } = await context.params; // WAJIB di-await
     const [year, kode_dokumen, query] = slug;
 
-    // const path = `Documens/${year}/${kode_dokumen}`;
-    const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+    const cookieKey = process.env.NODE_ENV === 'production' ? '__Secure-authjs.session-token' : 'authjs.session-token';
+    const jwt = await getToken({ req, secret: process.env.AUTH_SECRET, salt: cookieKey, cookieName: cookieKey });
+    const token = jwt?.accessToken;
     const url = `${process.env.API_URL}/view-pdf?name=${query}.pdf&tahun=${year}&kode_dokumen=${kode_dokumen}`;
-    // try {
-    // const results = await cloudinary.search
-    //     .expression(`${query} AND asset_folder:${path}`)
-    //     .execute();
 
-    // const cloudinaryUrl = results.resources[0]?.url;
-    // if (!cloudinaryUrl) {
-    //     return Response.json(
-    //         { title: "File PDF tidak ditemukan", message: "Silahkan Upload File PDF terlebihdahulu!" },
-    //         { status: 404 }
-    //     );
-    // }
 
     const pdfResponse = await fetch(`${url}`,
         {
             headers: {
-                Authorization: `Bearer ${token?.accessToken}`
+                Authorization: `Bearer ${token}`
             }
         }
     );
-    // if (!pdfResponse.ok) {
-    //     return Response.json(
-    //         { title: "File PDF tidak ditemukan", message: "Silahkan Upload File PDF terlebihdahulu!" },
-    //         { status: 404 }
-    //     );
-    // }
 
     const stream = pdfResponse.body;
-    //  const stream ="http://skitpbapp.work/storage/documens/2025/23/004624.pdf"
 
     return new Response(stream, {
         status: 200,
@@ -49,10 +31,5 @@ export async function GET(
             "Content-Disposition": `inline; filename=${query}.pdf`,
         },
     });
-    //     } catch (error) {
-    //         return Response.json(
-    //             { title: "File PDF tidak ditemukan", message: "Silahkan Upload File PDF terlebihdahulu!", error },
-    //             { status: 404 }
-    //         );
-    //     }
+
 }
